7583ba9f55897185dddd46ee070956e9b76fc7e7
Kang Seonghoon <public+hg@mearie.org>
2009-12-25 20:36:58 +0900

    experimental MemoryState implementation and propagate optimization pass rewriting

--- bfc/opt/propagate.py
+++ bfc/opt/propagate.py
@@ -3,6 +3,7 @@
 from bfc.nodes import *
 from bfc.expr import *
 from bfc.cond import *
+from bfc.memstate import *
 
 from bfc.opt.base import BaseOptimizerPass, Transformer
 from bfc.opt.cleanup import cleanup
@@ -13,93 +14,39 @@ class OptimizerPass(BaseOptimizerPass):
     # will act as memory blocker.
 
     def _transform(self, node):
-        backrefs = {}
-        usedrefs = {}
-        substs = {} # only for simple one, unless some vars are not current
+        state = MemoryState()
+        substs = {} # special casing for self-recursion; applied before memstate
 
-        tr = Transformer(node)
-        for i, cur in tr:
-            cur.withmemory(substs)
-
-            alters = mergable = False
-            offset = None
-            refs = []
+        result = []
+        for cur in node:
             if isinstance(cur, Nop):
                 pass
             elif isinstance(cur, SetMemory):
-                alters = mergable = True
-                offset = cur.offset
-                if cur.value.simple():
-                    substs[offset] = cur.value
-                elif cur.delta.simple():
-                    if offset in substs:
-                        substs[offset] += cur.delta
-                        if substs[offset].simple():
-                            # replace with equivalent SetMemory node.
-                            cur = SetMemory(offset, substs[offset])
-                            tr.replace(cur)
-                        else:
-                            del substs[offset]
-                else:
-                    try: del substs[offset]
-                    except: pass
+                result += state.set(cur.offset, cur.value)
             elif isinstance(cur, Input):
-                alters = True
-                offset = cur.offset
-                try: del substs[offset]
-                except: pass
+                flushed, offset = state.remove(cur.offset)
+                result += flushed
+                result.append(Input(offset))
             elif isinstance(cur, Output):
-                pass
+                flushed, expr = state.get(cur.expr)
+                assert not flushed
+                result.append(Output(expr))
             else: # MovePointer, While etc.
-                backrefs.clear()
-                usedrefs.clear()
-                substs.clear()
-                if isinstance(cur, (While, If)) and isinstance(cur.cond, MemNotEqual):
-                    substs[cur.cond.target] = cur.cond.value
-                elif isinstance(cur, SeekMemory):
-                    substs[cur.target] = cur.value
+                for offset, value in substs.items():
+                    result.append(SetMemory(offset, value))
+                result += state.flush()
+                if isinstance(cur, ComplexNode):
+                    cur = self._transform(cur)
+                result.append(cur)
 
-            refs = cur.postreferences().unsure
-            merged = False
-            if alters:
-                if not mergable:
-                    # prohibit next merging attempt.
-                    try: del backrefs[offset]
-                    except: pass
-                else:
-                    # we can merge node[target] and node[i] if:
-                    # - no operation has changed cell k between them. (thus such target
-                    #   is backrefs[offset], as it is updated after change)
-                    # - no operation has referenced the target cell between them.
-                    #   node[target] itself could reference that cell.
-                    # - no operation has changed cell(s) referenced by value.
-                    #   similar to above, node[target] is excluded from this rule.
-                    if offset in backrefs:
-                        target = backrefs[offset]
-                        if target >= usedrefs.get(offset, -1) and \
-                                all(target >= backrefs.get(ioffset, -1) for ioffset in refs):
-                            if isinstance(cur, SetMemory) and cur.delta.simple():
-                                if node[target].delta.simple():
-                                    node[target].value += cur.delta
-                                else:
-                                    node[target].delta += cur.delta
-                                if not node[target]:
-                                    node[target] = Nop()
-                            else:
-                                node[target] = cur
-                            tr.replace()
-                            merged = True
-
-                    if not merged:
-                        backrefs[offset] = i
-
-            if not merged:
-                target = i
-            for ioffset in refs:
-                usedrefs[ioffset] = target
-
+        for offset, value in substs.items():
+            result.append(SetMemory(offset, value))
+        result += state.flush()
+        print node
+        node[:] = result
+        print node
         return cleanup(node)
 
     def transform(self, node):
-        return self.visit(node, self._transform)
+        return self._transform(node)
 
